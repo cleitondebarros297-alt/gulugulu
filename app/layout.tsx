3db5fd464c1b082b6d89f9e38894c94f02912c75
cleiton de barros <cleitondebarros297@gmail.com>
2026-06-09 09:08:55 -0100

    c

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,12 +31,10 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
-       <ApolloClientProvider>
+      <ApolloClientProvider>
         <Toaster position="top-right" richColors expand />
           {children}
         </ApolloClientProvider>
-
-       
       </body>
     </html>
   );
